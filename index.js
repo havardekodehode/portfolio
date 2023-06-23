8684c87d1eea3c1bfeb992ef1d2b439848999449
havardekodehode <havarde.kodehode@gmail.com>
2023-06-23 15:06:09 +0200

    Made some progress on animations

--- index.js
+++ index.js
@@ -2,24 +2,23 @@ const backgroundEl = document.getElementById("background")
 
 
 function background(elements){
-    for (let i = 0; i < elements; i++) {
+    for (let i = 1; i < elements; i++) {
         const div = document.createElement("div")
         // const posx = Math.floor(Math.random()*100+1)
         // const posy = Math.floor(Math.random()*100+1)
         // data-posX="${posx}" data.posY="${posy}"
         const example = Math.floor(Math.random()*10+1)
-        // div.innerHTML = `
-        // <div class="background b${i}" >
-        // </div>
-        // // `
-        div.style.animationName = `example${example}`
-        div.style.height = `${i}00`
-        div.className= `background b${i}`
-        backgroundEl.append(div)
-        console.log(i + " " + `
-        <div class="background b${example}" >
-        </div>
-        // `);
+        // div.style.animationName = `example${example}`
+        div.style.animationName = `example1`
+        div.style.transform = `translateY(${i*11}rem) translateX(${-example*2}rem) rotate(130deg)`
+        // div.style.left = "-1000px"
+        div.style.height = `${example}00`
+        div.className= `background b1 `
+        setTimeout(()=>{
+            backgroundEl.append(div)
+
+        }, example * 200)
+        // i===1?div.style.backgroundColor="red":console.log("nothing");
     }
     console.log("heeei")
     
